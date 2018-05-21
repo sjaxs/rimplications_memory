22a6ba34c429aec296d1763999ae460da3b339fd
jesus <invitado32@gmail.com>
2018-05-21 20:24:34 +0200

    description for graphics in redundancy extended, added graphics comparision to closure

--- r_code/closure.R
+++ r_code/closure.R
@@ -5,17 +5,13 @@ apply.closure <- function(Sigma,Xmas){
   while (!fixpoint){ 
     fixpoint <- TRUE
     cont <- cont + 1
-     if(cont == 2){
-       Xmas <- difference.sets(Xmas, Xmas2)
-     }
+    if(cont == 2) Xmas <- difference.sets(Xmas, Xmas2)
     k <- 1
     l <- length(Sigma)
     while (k <= l){  # top -> Xmas,  A -> B 
       A <- read.left(Sigma,k)
       B <- read.right(Sigma,k) 
-       if(is.empty(Xmas)){
-         break
-       }
+      if(is.empty(Xmas)) break
       newL <- difference.sets(A,Xmas) 
       if (is.empty(newL)) {
         Xmas  <- union.sets(B,Xmas)
@@ -42,4 +38,3 @@ apply.closure <- function(Sigma,Xmas){
   }#end while
   return(list("closure"=union.sets(Xmas,Xmas2), "implications"=Sigma))
 }#End apply.closure
-
