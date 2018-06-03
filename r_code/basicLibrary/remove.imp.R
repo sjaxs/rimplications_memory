cec3b3d99b8e3452180a9bf2d20993735fb3222b
Anavillalon <anaesperanzavillalon@hotmail.com>
2018-06-03 13:56:09 +0200

    Done until add implication

--- r_code/basicLibrary/remove.imp.R
+++ r_code/basicLibrary/remove.imp.R
@@ -1,4 +1,3 @@
 remove.imp <- function(Sigma, k){
-  rNJ <- new("rules", lhs=lhs(Sigma[k]), rhs=itemSetdiff(lhs(Sigma[k]), lhs(Sigma[k])), quality = data.frame(confidence = 1))
-  return(c(Sigma[seq_len(k-1)], rNJ, Sigma[k+seq_len(length(Sigma)-k)]))
+  return(Sigma[-k])
 }#End remove.imp
