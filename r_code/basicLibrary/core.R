9211ddb72c8bdb9fa38f7ffe670e950ce2007782
Anavillalon <anaesperanzavillalon@hotmail.com>
2018-06-20 16:20:54 +0200

    Fixing errors

--- r_code/basicLibrary/core.R
+++ r_code/basicLibrary/core.R
@@ -1,8 +1,8 @@
 core <- function(Omega, Gamma){
-  rpSet <- read.right(Gamma,1)
+  rpSet <- read.right(Gamma, 1)
   for(k in seq(Gamma)){
-    B <- read.right(Gamma,k)
-    rpSet <- union.sets(rpSet,B)
+    B <- read.right(Gamma, k)
+    rpSet <- union.sets(rpSet, B)
   }
   newOmega <- difference.sets(Omega, rpSet)
   return(newOmega)
